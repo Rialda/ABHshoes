c15aebc76b901cc72d7c502fd71efb53f66c5efc
Rialda <rialda.spahic@gmail.com>
2017-04-18 17:26:44 +0200

    Products table

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,27 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170416172944) do
+ActiveRecord::Schema.define(version: 20170418141221) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "products", force: :cascade do |t|
+    t.string   "name",                null: false
+    t.string   "brand",               null: false
+    t.string   "size"
+    t.string   "color"
+    t.string   "style"
+    t.string   "category",            null: false
+    t.decimal  "price",               null: false
+    t.integer  "num_of_products"
+    t.datetime "created_at",          null: false
+    t.datetime "updated_at",          null: false
+    t.string   "avatar_file_name"
+    t.string   "avatar_content_type"
+    t.integer  "avatar_file_size"
+    t.datetime "avatar_updated_at"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
@@ -28,6 +45,7 @@ ActiveRecord::Schema.define(version: 20170416172944) do
     t.inet     "last_sign_in_ip"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
+    t.string   "username"
     t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
