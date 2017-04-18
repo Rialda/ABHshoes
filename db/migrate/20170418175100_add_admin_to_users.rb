7b3fd6b69064b01995372c7ac6a5836c26b332b3
Rialda <rialda.spahic@gmail.com>
2017-04-18 22:02:14 +0200

    Admin column and polished navigation

--- db/migrate/20170418175100_add_admin_to_users.rb
+++ db/migrate/20170418175100_add_admin_to_users.rb
@@ -1,6 +1,6 @@
 class AddAdminToUsers < ActiveRecord::Migration[5.0]
   def change
-    add_column :users, :admin, :boolean, default: false #adds username column to users of type string
+  #  add_column :users, :admin, :boolean, default: true #adds username column to users of type string
 
   end
 end
--- db/migrate/20170418175726_add_default_value_to_admin.rb
+++ db/migrate/20170418175726_add_default_value_to_admin.rb
@@ -1,5 +1,4 @@
 class AddDefaultValueToAdmin < ActiveRecord::Migration[5.0]
   def change
-    change_column :users, :admin, :boolean, :default => false
   end
 end
