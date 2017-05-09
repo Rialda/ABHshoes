6813c1254f92bc0b46f5c188bfe680762b3ea62f
Rialda <rialda.spahic@gmail.com>
2017-05-10 01:12:34 +0200

    features

--- db/migrate/20170425104556_product.rb
+++ db/migrate/20170425104556_product.rb
@@ -1,7 +1,7 @@
 class Product < ActiveRecord::Migration[5.0]
   def change
-    integer :color_id, array: true # adding array fields works only starting from Rails 4
-    index :color_id, using: :gin   # we add GIN index to speed up specific queries on array
+    # integer :color_id, array: true # adding array fields works only starting from Rails 4
+    # index :color_id, using: :gin   # we add GIN index to speed up specific queries on array
 
   end
 end
